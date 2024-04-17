6d3be3421b48a4f2775d5dc4882dd0593adf4af6
portezera <134521956+portezera@users.noreply.github.com>
2024-04-17 11:38:01 -0300

    a

--- code.js
+++ code.js
@@ -1,1 +1,27 @@
+//Boolean: armazena dois valores, true ou false
+let x = true
+console.log(x)
+
+//Number: armazena números inteiros, decimais ou hexadecimais
+let a = 20
+console.log(a)
+
+//String: armazena texto
+let texto = ('ola Mundo') 
+console.log(texto)
+
+//Undefined: quando não há nenhum valor atribuído
+let b = 
+console.log(b)
+
+//Null: armazena um valor vazio
+let c =()
+console.log(c)
+
+/*BigInt é um objeto nativo que fornece um modo de representar números inteiros maiores que 253,
+ que é o maior número que o JavaScript consegue,
+  com exatidão, representar com o tipo primitivo Number.
+*/
+BigInt(500)
+console.log(BigInt)
 
